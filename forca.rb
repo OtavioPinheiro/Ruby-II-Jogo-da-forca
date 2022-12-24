db2f62dcc1b024c8b13d71350dbd340983b464e5
Otávio Pinheiro <otavioaecpinheiro@gmail.com>
2022-12-24 12:17:03 -0300

    feat: Separando ui da bl

--- forca.rb
+++ forca.rb
@@ -1,34 +1,4 @@
-def da_boas_vindas
-  puts "Bem vindo ao jogo da forca"
-  puts "Qual é o seu nome?"
-  nome = gets.strip
-  puts "\n\n\n"
-  puts "Começaremos o jogo para você, #{nome}."
-  nome
-end
-
-def escolhe_palavra_secreta
-  puts "Escolhendo uma palavra secreta..."
-  palavra_secreta = "programador"
-  puts "Palavra secreta com #{palavra_secreta.size} letras ... boa sorte!"
-  palavra_secreta
-end
-
-def nao_quer_jogar?
-  puts "Deseja jogar novamente? (S/N)"
-  quero_jogar = gets.strip
-  nao_quero_jogar = quero_jogar.upcase == "N"
-end
-
-def pede_um_chute(chutes, erros)
-  puts "\n\n"
-  puts "Erros até agora: #{erros}"
-  puts "Chutes até agora: #{chutes}"
-  puts "Entre com uma letra ou uma palavra"
-  chute = gets.strip
-  puts "Será que acertou? Você chutou #{chute}"
-  chute
-end
+require_relative 'ui'
 
 def joga(nome)
   palavra_secreta = escolhe_palavra_secreta
@@ -39,7 +9,7 @@ def joga(nome)
   while erros < 5
     chute = pede_um_chute(chutes, erros)
     if chutes.include? chute
-      puts "Você já chutou #{chute}"
+      avisa_chute_efetuado(chute)
       next
     end
     chutes << chute
@@ -48,25 +18,25 @@ def joga(nome)
     if chutou_uma_letra
       total_encontrado = palavra_secreta.count(chute)
       if total_encontrado == 0
-        puts "Letra não encontrada."
+        avisa_letra_nao_encontrada
         erros += 1
       else
-        puts "Letra encontrada #{total_encontrado} vezes."
+        avisa_letra_encontrada(total_encontrado)
       end
     else
       acertou = chute == palavra_secreta
       if acertou
-        puts "Parabéns! Acertou!"
+        avisa_acertou_palavra
         pontos_ate_agora += 100
         break
       else
-        puts "que pena... errou"
+        avisa_errou_palavra
         pontos_ate_agora -= 30
         erros += 1
       end
     end
   end
-  puts "Você ganhou #{pontos_ate_agora} pontos."
+  avisa_pontos(pontos_ate_agora)
 end
 
 nome = da_boas_vindas
