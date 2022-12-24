a85d55f65f90c80be06248a240be67ff5c49f2d0
Otávio Pinheiro <otavioaecpinheiro@gmail.com>
2022-12-24 14:32:13 -0300

    feat: Lógica para ler os dados de rank.txt

--- ui.rb
+++ ui.rb
@@ -2,6 +2,10 @@ def avisa_pontos_totais(pontos_totais)
   puts "Você possui #{pontos_totais} pontos."
 end
 
+def avisa_campeao_atual(dados)
+  puts "Nosso campeão atual é #{dados[0]} com #{dados[1]} pontos."
+end
+
 def avisa_chute_efetuado(chute)
   puts "Você já chutou #{chute}"
 end
