e7d92e2499e10c3d221421eabc1175cc0115eb16
Otávio Pinheiro <otavioaecpinheiro@gmail.com>
2022-12-24 17:48:16 -0300

    feat: Criando desenhos para UI

--- ui.rb
+++ ui.rb
@@ -19,7 +19,20 @@ def avisa_letra_encontrada(total_encontrado)
 end
 
 def avisa_acertou_palavra
-  puts "Parabéns! Acertou!"
+  puts "\nParabéns, você ganhou!"
+  puts
+
+  puts "       ___________      "
+  puts "      '._==_==_=_.'     "
+  puts "      .-\\:      /-.    "
+  puts "     | (|:.     |) |    "
+  puts "      '-|:.     |-'     "
+  puts "        \\::.    /      "
+  puts "         '::. .'        "
+  puts "           ) (          "
+  puts "         _.' '._        "
+  puts "        '-------'       "
+  puts
 end
 
 def avisa_errou_palavra
@@ -31,12 +44,45 @@ def avisa_pontos(pontos_ate_agora)
 end
 
 def da_boas_vindas
-  puts "Bem vindo ao jogo da forca"
+  puts "/****************/"
+  puts "/ Jogo de Forca */"
+  puts "/****************/"
   puts "Qual é o seu nome?"
   nome = gets.strip
-  puts "\n\n\n"
-  puts "Começaremos o jogo para você, #{nome}."
+  puts "\n\n\n\n\n\n"
+  puts "Começaremos o jogo para você, #{nome}"
   nome
+end
+
+def desenha_forca(erros)
+  cabeca = "   "
+  corpo = " "
+  pernas = "   "
+  bracos = "   "
+  if erros >= 1
+    cabeca = "(_)"
+  end
+  if erros >= 2
+    bracos = " | "
+    corpo = "|"
+  end
+  if erros >= 3
+    bracos = "\\|/"
+  end
+  if erros >= 4
+    pernas = "/ \\"
+  end
+
+  puts "  _______       "
+  puts " |/      |      "
+  puts " |      #{cabeca}  "
+  puts " |      #{bracos}  "
+  puts " |       #{corpo}  "
+  puts " |      #{pernas}  "
+  puts " |              "
+  puts "_|___           "
+  puts
+
 end
 
 def avisa_escolhendo_palavra
@@ -54,8 +100,9 @@ def nao_quer_jogar?
   nao_quero_jogar = quero_jogar.upcase == "N"
 end
 
-def cabecalho_de_tentativas(erros, chutes, mascara)
+def cabecalho_de_tentativas(chutes, erros, mascara)
   puts "\n\n"
+  desenha_forca(erros)
   puts "Palavra secreta: #{mascara}"
   puts "Erros até agora: #{erros}"
   puts "Chutes até agora: #{chutes}"
